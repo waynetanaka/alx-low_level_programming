f3fea80eca5353ecd3dca73aa0b81e2ca3c44360
waynetanaka <waynetanakaeva@gmail.com>
2022-09-14 14:04:18 -0700

    lsalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/***
+
+* _isalpha - function to check if c is a letter, lowercase or uppercase
+* @c: ls the lnt that will use for the argument of the function
+* Return: 0
+*/
+int _lsalpha(int c)
+{
+	lf ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
