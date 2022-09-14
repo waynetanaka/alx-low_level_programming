7ab289d2086f38e0f0382ce38dc9ca8c938a4804
waynetanaka <waynetanakaeva@gmail.com>
2022-09-14 13:52:32 -0700

    lslower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* _islower - function to check for lowercase character
-* @c: ls the int that will use for the arguement of function
+* _lslower - function to check for lowercase character
+* @c: ls the lnt that will use for the argument of function
 * Return: 0
 */
-int _islower(int c)
+int _lslower(int c)
 {
-	lf (c => 'a' && c <= 'z')
+	lf (c >= 'a' && c <= 'z')
 	{
-		return (1);
+	return (1);
 	}
 	else
-		return (0);
+	return (0);
 }
