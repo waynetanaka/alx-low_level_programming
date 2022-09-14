23de26c141b4af6f6cd6189d4134062495689cd3
waynetanaka <waynetanakaeva@gmail.com>
2022-09-14 13:03:55 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,12 @@
 * Return: 0
 */
 void print_alphabet(void)
-{
+{ 
 	char c;
 
-	for (c='a'; c<='z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-	_putchar(c);
+		_putchar(c);
+		_putchar('\n');
 	}
-_putchar('\n');
+}
