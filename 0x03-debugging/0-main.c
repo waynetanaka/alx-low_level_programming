c15eb25f20a1b42722bda657bfc3c91c875c175c
waynetanaka <waynetanakaeva@gmail.com>
2022-09-15 17:57:29 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - test function that prints if the integer is positive_or_negative
+* Return: 0
+*/
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,4 @@
+0. Multiple mains
+1. Like, comment, subscribe
+2. This program prints the largest of three integers
+3. This program converts a date to the day of year
