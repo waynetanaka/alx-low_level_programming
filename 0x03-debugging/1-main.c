cfa6563dc7dfb4803cc6b1dfb0dfc59c9675ba66
waynetanaka <waynetanakaeva@gmail.com>
2022-09-15 18:08:10 -0700

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - causes on infinite loop
+* Return: 0
+*/
+int main(void)
+{
+int i;
+
+printf("lnfinte loop lncoming :(\n");
+
+i = 0;
+/*
+* while (i < 10)
+* {
+* putchar(i);
+* }
+*/
+printf("lnfinite loop avoided! \\o/\n");
+
+return (0);
+}
